b22dfa9fa10df085a5cc839730c78c0b4e1633f5
kneeraazon01 <karkinirajan1999@gmail.com>
2020-12-06 19:06:29 +0545

    written a django model using TDD with the unit tests

--- app/app/calc.py
+++
@@ -1,8 +0,0 @@
-def add(x, y):
-    """Add two numbers together and return the result"""
-    return x + y
-
-
-def subtract(x, y):
-    """Subtract x from y and return result"""
-    return y - x
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+    """ This tests the creating user with the user email successfully"""
+
+    def test_create_user_with_email_successful(self):
+        email = "test@gmail.com"
+        password = "test123"
+        user = get_user_model().objects.create_user(email=email, password=password)
+        self.assertEquals(user.email, email)
+        self.assertTrue(user.check_password(password))
+        # ? thats is because the password will be encrypted
